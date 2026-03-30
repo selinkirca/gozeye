daa2d229f5ee31652a23107c375cf20b9f2cb7f9
selinkirca <selin.kirca.41@gmail.com>
2026-03-30 15:10:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -198,6 +198,7 @@ Geliştirilen bu sistem, %91.4 doğruluk oranı ile göz hastalıklarının erke
 \n**Kaynakça:**
 - Sandler, M., et al. (2018). MobileNetV2: Inverted Residuals and Linear Bottlenecks.
 - Kaggle: Eye Disease Dataset (Küratörlü medikal veriler).
+https://www.kaggle.com/datasets/gunavenkatdoddi/eye-diseases-classification
 - TensorFlow & Keras API Dokümantasyonu.
 - Reza, A. M. (2004). Realization of the Contrast Limited Adaptive Histogram Equalization (CLAHE) for Real-Time Image Enhancement. Journal of VLSI Signal Processing.
 https://jamanetwork.com/journals/jama/fullarticle/2588763
